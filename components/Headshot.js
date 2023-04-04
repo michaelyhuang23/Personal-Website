6f575674cd8e45db7af28cb03d0bf46ad1843afc
Michael Huang <michaelyhuang23@gmail.com>
2023-04-03 20:48:04 -0400

    tiny update

--- components/Headshot.js
+++ components/Headshot.js
@@ -2,7 +2,15 @@ import styles from "../styles/index.module.css";
 
 const Headshot = ({title, url}) => {
     return (
-        <img src="/photo.jpg" className={styles.headshot}></img>
+        <div className={styles.headshot}>
+        <img src="/photo.jpg" className={styles.headshotImg}></img>
+        <p>I would love to chat if you are interested in AI or alignment work, or philosophy!</p>
+        <ul>
+            <li>Twitter: <a href="https://twitter.com/michael_yhuang">@michael_yhuang</a></li>
+            <li>Discord: Michael-H#4622</li>
+            <li>Email: michaelyhuang23 at gmail dot com</li>
+        </ul>
+        </div>
     )
 }
 
